fe1337fe18ed7149f7556c4e701f397f9043d7c0
Hemant Agrawal <hemant.agrawal@nxp.com>
2016-07-21 21:42:46 +0530

    dpaa: Integrate DPAA PMD with build process
    DPAA driver is based on the rte_eal_init() hook for SoC initialization, common
    with DPAA2 driver. This patch introduces the build process changes to compile
    the PMD driver with this hook.
    
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>
    Signed-off-by: Ramesh Iyer <ramesh.iyer@nxp.com>
    Signed-off-by: Xuelin Shi <xuelin.shi@nxp.com>
    DPDK-121

--- mk/rte.app.mk
+++ mk/rte.app.mk
@@ -103,6 +103,14 @@ _LDLIBS-$(CONFIG_RTE_LIBRTE_PMD_BOND)       += -lrte_pmd_bond
 _LDLIBS-$(CONFIG_RTE_LIBRTE_PMD_XENVIRT)    += -lrte_pmd_xenvirt -lxenstore
 _LDLIBS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD)	    += -lrte_pmd_dpaa2
 
+ifeq ($(CONFIG_RTE_LIBRTE_DPAA_PMD),y)
+	_LDLIBS-$(CONFIG_RTE_LIBRTE_DPAA_PMD) += -lz
+	_LDLIBS-$(CONFIG_RTE_LIBRTE_DPAA_PMD) += -L$(XML_ZLIB_PATH) -lxml2
+	_LDLIBS-$(CONFIG_RTE_LIBRTE_DPAA_PMD) += -lm
+	_LDLIBS-$(CONFIG_RTE_LIBRTE_DPAA_PMD) += -lnxp_usdpaa
+	_LDLIBS-$(CONFIG_RTE_LIBRTE_DPAA_PMD) += -lrte_pmd_dpaa
+endif
+
 ifeq ($(CONFIG_RTE_BUILD_SHARED_LIB),n)
 # plugins (link only if static libraries)
 
