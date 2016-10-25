e0389957f54246a2646b1a78f675072b03130b89
Nipun Gupta <nipun.gupta@nxp.com>
2016-10-25 23:33:33 +0530

    dpaa1: Discard the Rx errors correctly for FMAN ports
    Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>
    DPDK-190

--- drivers/net/dpaa/dpaa_ethdev.c
+++ drivers/net/dpaa/dpaa_ethdev.c
@@ -898,6 +898,7 @@ static int dpaa_eth_dev_init(struct rte_eth_dev *eth_dev)
 	eth_dev->data->mac_addrs = (struct ether_addr *)dpaa_intf->mac_addr;
 
 	/* Disable RX, disable promiscuous mode */
+	fman_if_discard_rx_errors(fman_intf);
 	fman_if_disable_rx(fman_intf);
 	fman_if_promiscuous_disable(fman_intf);
 	fman_if_stats_reset(fman_intf);
