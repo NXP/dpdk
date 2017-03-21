0a17438b4bb9970b5b2d133fea2833d52e8db141
Shreyansh Jain <shreyansh.jain@nxp.com>
2017-03-21 16:05:05 +0530

    dpaa2: fix legal copyright for MC files
    Change "Copyright 2017 NXP USA Inc." to "Copyright 2017 NXP"
    
    Signed-off-by: Shreyansh Jain <shreyansh.jain@nxp.com>
    DPDK-478

--- drivers/bus/fslmc/mc/dpkg.c
+++ drivers/bus/fslmc/mc/dpkg.c
@@ -4,7 +4,7 @@
  *
  *   BSD LICENSE
  *
- * Copyright 2016 NXP USA, Inc.
+ * Copyright 2017 NXP
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
