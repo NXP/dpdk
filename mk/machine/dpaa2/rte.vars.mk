98f548c36b3c805a04d9963b8eb02e09340aa089
Akhil Goyal <akhil.goyal@nxp.com>
2017-06-23 16:21:06 +0530

    dpaa2: fix incorrect copyright string
    Signed-off-by: Akhil Goyal <akhil.goyal@nxp.com>
    DPDK-630

--- mk/machine/dpaa2/rte.vars.mk
+++ mk/machine/dpaa2/rte.vars.mk
@@ -1,7 +1,8 @@
 #   BSD LICENSE
 #
 #   Copyright (c) 2015 Freescale Semiconductor, Inc. All rights reserved.
-#   Copyright (c) 2016 NXP. All rights reserved.
+#   Copyright 2016 NXP.
+#   All rights reserved.
 #
 #   Redistribution and use in source and binary forms, with or without
 #   modification, are permitted provided that the following conditions
