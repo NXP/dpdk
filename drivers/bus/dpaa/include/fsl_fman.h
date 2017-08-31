0aec984518206826d8145ea6e8064f6e2e43fe89
Hemant Agrawal <hemant.agrawal@nxp.com>
2017-08-31 14:59:30 +0530

    net/dpaa2: add support for extra stats
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>
    DPDK-924

--- drivers/bus/dpaa/include/fsl_fman.h
+++ drivers/bus/dpaa/include/fsl_fman.h
@@ -90,6 +90,9 @@ void fman_if_stats_get(struct fman_if *p, struct rte_eth_stats *stats);
 /* Reset the FMAN statistics */
 void fman_if_stats_reset(struct fman_if *p);
 
+/* Get all of the FMAN statistics */
+void fman_if_stats_get_all(struct fman_if *p, uint64_t *value, int n);
+
 /* Set ignore pause option for a specific interface */
 void fman_if_set_rx_ignore_pause_frames(struct fman_if *p, bool enable);
 
