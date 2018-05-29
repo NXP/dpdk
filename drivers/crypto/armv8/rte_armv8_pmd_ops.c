e324b1f9b2d7aa3a59652a095e11ea91d49fb04d
Anoob Joseph <anoob.joseph@caviumnetworks.com>
2018-05-29 16:15:36 +0530

    crypto/armv8: fix HMAC supported digest sizes
    For HMAC algorithms (SHAx-HMAC), the supported digest sizes are not a
    fixed value, but a range between 1 and the maximum digest size for those
    algorithms.
    
    Fixes: 0f89def76127 ("crypto/armv8: fix HMAC supported key sizes")
    Cc: stable@dpdk.org
    
    Signed-off-by: Anoob Joseph <anoob.joseph@caviumnetworks.com>
    Signed-off-by: Sachin Saxena <sachin.saxena@nxp.com>
    Acked-by: Akhil Goyal <akhil.goyal@nxp.com>

--- drivers/crypto/armv8/rte_armv8_pmd_ops.c
+++ drivers/crypto/armv8/rte_armv8_pmd_ops.c
@@ -55,9 +55,9 @@ static const struct rte_cryptodev_capabilities
 						.increment = 1
 					},
 					.digest_size = {
-						.min = 20,
+						.min = 1,
 						.max = 20,
-						.increment = 0
+						.increment = 1
 					},
 					.iv_size = { 0 }
 				}, }
@@ -76,9 +76,9 @@ static const struct rte_cryptodev_capabilities
 						.increment = 1
 					},
 					.digest_size = {
-						.min = 32,
+						.min = 1,
 						.max = 32,
-						.increment = 0
+						.increment = 1
 					},
 					.iv_size = { 0 }
 				}, }
