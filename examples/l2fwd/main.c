c05384b9d3bca552ad3b03878ed30e5cd93df308
Hemant Agrawal <hemant.agrawal@nxp.com>
2018-08-03 16:25:25 +0530

    PVT:examples/l2fwd: burst size as a argument
    use -b as a argument for burst size for application.
    
    It shall be 7 for dpaa1
    
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>
    DPDK-1110

--- examples/l2fwd/main.c
+++ examples/l2fwd/main.c
@@ -82,6 +82,7 @@ static int mac_updating = 1;
 #define BURST_TX_DRAIN_US 100 /* TX drain every ~100us */
 #define MEMPOOL_CACHE_SIZE 256
 
+static int max_burst_size = MAX_PKT_BURST;
 /*
  * Configurable number of RX/TX ring descriptors
  */
@@ -305,7 +306,7 @@ l2fwd_main_loop(void)
 
 			portid = qconf->rx_port_list[i];
 			nb_rx = rte_eth_rx_burst(portid, 0,
-						 pkts_burst, MAX_PKT_BURST);
+						 pkts_burst, max_burst_size);
 
 			port_statistics[portid].rx += nb_rx;
 
@@ -352,7 +353,8 @@ l2fwd_usage(const char *prgname)
 		   "  --[no-]mac-updating: Enable or disable MAC addresses updating (enabled by default)\n"
 		   "      When enabled:\n"
 		   "       - The source MAC address is replaced by the TX port MAC address\n"
-		   "       - The destination MAC address is replaced by 02:00:00:00:00:TX_PORT_ID\n",
+		   "       - The destination MAC address is replaced by 02:00:00:00:00:TX_PORT_ID\n"
+		"  -b NUM: burst size for receive packet (default is 32)\n",
 	       prgname);
 }
 
@@ -411,6 +413,7 @@ static const char short_options[] =
 	"p:"  /* portmask */
 	"q:"  /* number of queues */
 	"T:"  /* timer period */
+	"b:"  /* burst size */
 	;
 
 #define CMD_LINE_OPT_MAC_UPDATING "mac-updating"
@@ -434,7 +437,7 @@ static const struct option lgopts[] = {
 static int
 l2fwd_parse_args(int argc, char **argv)
 {
-	int opt, ret, timer_secs;
+	int opt, ret, timer_secs, burst_size;
 	char **argvopt;
 	int option_index;
 	char *prgname = argv[0];
@@ -474,6 +477,17 @@ l2fwd_parse_args(int argc, char **argv)
 				return -1;
 			}
 			timer_period = timer_secs;
+			break;
+
+		/* max_burst_size */
+		case 'b':
+			burst_size = (unsigned int)atoi(optarg);
+			if (burst_size < 0 || burst_size > max_burst_size) {
+				printf("invalid burst size\n");
+				l2fwd_usage(prgname);
+				return -1;
+			}
+			max_burst_size = burst_size;
 			break;
 
 		/* long options */
