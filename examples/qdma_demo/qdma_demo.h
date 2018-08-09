ef2dc340ef01a700510f1db94d89ab856b85a369
Pankaj Chauhan <pankaj.chauhan@nxp.com>
2018-08-09 21:42:06 +0530

    qdma_demo: Adding qdma_demo application
    Add qdma_demo application. Following is tested
    on LX2 & LS2088:
    1. MEM_TO_PCI transfers of varying sizes (64-1024 bytes)
    2. qdma_demo runs on PCI RC.
    
    In above mentioned test cases EP is LX2RDB booted till
    u-boot prompt.
    
    Original code taken from Yi Zhao's repository:
    ssh://git@bitbucket.sw.nxp.com/~nxa07649/dpdk-rbp.git
    branch - debug-rbp
    
    Signed-off-by: Yi Zhao <yi.zhao@nxp.com>
    Signed-off-by: Pankaj Chauhan <pankaj.chauhan@nxp.com>

---
+++ examples/qdma_demo/qdma_demo.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2018 NXP
+ */
+#ifndef __QDMA_DEMO_H__
+#define __QDMA_DEMO_H__
+
+#define QDMA_MAX_HW_QUEUES_PER_CORE	1
+#define QDMA_FLE_POOL_COUNT		8192
+#define QDMA_MAX_VQS			128
+
+#define TEST_PCIE_32B_WR 0
+
+#define TEST_PCIE_READ 1
+#define TEST_PCIE_32B 1
+
+#if TEST_PCIE_READ
+#define TEST_PCIE_READ_TIMES 10000000
+#else /*write times*/
+#define TEST_PCIE_READ_TIMES 100000000
+#endif
+
+#define PAGE_SIZE	(sysconf(_SC_PAGESIZE))
+#define PAGE_MASK	(~(PAGE_SIZE - 1))
+
+#define ULTRA_SHORT_FMT 1
+#define USE_RBP 1
+#define NO_RBP 0
+#define LONG_FMT 0
+#define PCI_TO_PCI 1
+#define MEM_TO_PCI 2
+#define PCI_TO_MEM 3
+#define MEM_TO_MEM 4
+
+#define TEST_DEQUEU_CNT 6
+#define TEST_PACKETS_NUM (g_packet_num)
+#define TEST_M_SIZE (1024)
+#define TEST_PCI_SIZE_LIMIT (8*TEST_M_SIZE)
+#define TEST_PCIBUS_BASE_ADDR (0x9060044000)
+#define TEST_PCICPU_BASE_ADDR (0x0 + TEST_PCIBUS_BASE_ADDR)
+#define TEST_PACKET_SIZE (g_packet_size)
+#define TEST_LOCAL_MEM_SIZE_PERCORE (TEST_PACKET_SIZE * TEST_PACKETS_NUM)
+
+#define LSINIC_QDMA_MAX_HW_QUEUES_PER_CORE	2
+#define LSINIC_QDMA_FLE_POOL_COUNT		 (2 * 8192)
+#define LSINIC_QDMA_MAX_VQS			2048
+
+extern struct qdma_virt_queue qdma_vqs[];
+
+#endif
