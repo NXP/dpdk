0c0332ae0016ca6742961f1f4ac6e2f4fdbe215a
Nipun Gupta <nipun.gupta@nxp.com>
2018-09-09 19:20:52 +0530

    PVT: test: add NXP Copyright to test cryptodev
    Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>

--- test/test/test_cryptodev.c
+++ test/test/test_cryptodev.c
@@ -2,6 +2,7 @@
  *   BSD LICENSE
  *
  *   Copyright(c) 2015-2017 Intel Corporation. All rights reserved.
+ *   Copyright 2018 NXP
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
