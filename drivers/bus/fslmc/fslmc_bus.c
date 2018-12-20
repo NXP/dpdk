296475e72b4ac2663600c1dcefb74e3c1451f018
Shreyansh Jain <shreyansh.jain@nxp.com>
2018-12-20 18:06:55 +0530

    bus/fslmc: add support for secondary processes
    Previously FSLMC bus only supported blacklisting of DPNI (eth),
    DPSECI (crypto) devices. With this patch, devices like DPIO,
    DPMCP, and other DP* can also be blacklisted/whitelisted.
    
    This is a required condition for secondary processes where the
    secondary needs to be passed a mutually exclusive list of
    resources as compared the primary and all other secondaries.
    
    This patch also moves the DPIO memory from malloc to hugepage so
    that in future in case the DPIO list can be shared, it can be
    accessed in secondaries.
    
    Once this patch is done, multi-process cases can be executed by
    whitelisting/blacklisting devices in each instance.
    
    Signed-off-by: Shreyansh Jain <shreyansh.jain@nxp.com>
    DPDK-1479

--- drivers/bus/fslmc/fslmc_bus.c
+++ drivers/bus/fslmc/fslmc_bus.c
@@ -396,12 +396,10 @@ rte_fslmc_probe(void)
 	/* Only in case of primary the VFIO group and dma-map setup would be
 	 * done.
 	 */
-	if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
-		ret = fslmc_vfio_process_group();
-		if (ret) {
-			DPAA2_BUS_ERR("Unable to setup devices %d", ret);
-			return 0;
-		}
+	ret = fslmc_vfio_process_group();
+	if (ret) {
+		DPAA2_BUS_ERR("Unable to setup devices %d", ret);
+		return 0;
 	}
 
 	probe_all = rte_fslmc_bus.bus.conf.scan_mode != RTE_BUS_SCAN_WHITELIST;
