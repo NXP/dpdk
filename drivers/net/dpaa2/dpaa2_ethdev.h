b010d557ade241b19581c28ff976c9189e3e5d51
Sunil Kumar Kori <sunil.kori@nxp.com>
2019-02-22 11:16:05 +0000

    net/dpaa2: support generic flow
    Signed-off-by: Sunil Kumar Kori <sunil.kori@nxp.com>
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- drivers/net/dpaa2/dpaa2_ethdev.h
+++ drivers/net/dpaa2/dpaa2_ethdev.h
@@ -89,6 +89,13 @@
 /* enable timestamp in mbuf*/
 extern enum pmd_dpaa2_ts dpaa2_enable_ts;
 
+#define DPAA2_QOS_TABLE_RECONFIGURE	1
+#define DPAA2_FS_TABLE_RECONFIGURE	2
+
+/*Externaly defined*/
+extern const struct rte_flow_ops dpaa2_flow_ops;
+extern enum rte_filter_type dpaa2_filter_type;
+
 struct dpaa2_dev_priv {
 	void *hw;
 	int32_t hw_id;
@@ -109,7 +116,22 @@ struct dpaa2_dev_priv {
 	uint8_t en_loose_ordered;
 	uint8_t max_cgs;
 	uint8_t cgid_in_use[MAX_RX_QUEUES];
+
+	struct pattern_s {
+		uint8_t item_count;
+		uint8_t pattern_type[DPKG_MAX_NUM_OF_EXTRACTS];
+	} pattern[MAX_TCS + 1];
+
+	struct extract_s {
+		struct dpkg_profile_cfg qos_key_cfg;
+		struct dpkg_profile_cfg fs_key_cfg[MAX_TCS];
+		uint64_t qos_extract_param;
+		uint64_t fs_extract_param[MAX_TCS];
+	} extract;
 };
+
+int dpaa2_distset_to_dpkg_profile_cfg(uint64_t req_dist_set,
+				      struct dpkg_profile_cfg *kg_cfg);
 
 int dpaa2_setup_flow_dist(struct rte_eth_dev *eth_dev,
 			  uint64_t req_dist_set);
