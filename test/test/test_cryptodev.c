fb47ac1d15a19d87b4051c43b9d339a3b706cc4e
Hemant Agrawal <hemant.agrawal@nxp.com>
2019-07-17 17:48:22 +0530

    test/crypto: add zuc auth test for dpaa2_sec
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- test/test/test_cryptodev.c
+++ test/test/test_cryptodev.c
@@ -11357,6 +11357,14 @@ static struct unit_test_suite cryptodev_dpaa2_sec_testsuite  = {
 		TEST_CASE_ST(ut_setup, ut_teardown,
 			test_zuc_encryption_test_case_5),
 
+		/** ZUC authenticate (EIA3) */
+		TEST_CASE_ST(ut_setup, ut_teardown,
+			test_zuc_hash_generate_test_case_6),
+		TEST_CASE_ST(ut_setup, ut_teardown,
+			test_zuc_hash_generate_test_case_7),
+		TEST_CASE_ST(ut_setup, ut_teardown,
+			test_zuc_hash_generate_test_case_8),
+
 		TEST_CASES_END() /**< NULL terminate unit test array */
 	}
 };
