a6d6791179dbb9e7ed538aa6ff96adbcb2e581fb
Sachin Saxena <sachin.saxena@nxp.com>
2019-11-06 15:51:22 +0530

    PVT: mempool: LS1043 errata porting
    add 4k boundary errata workaround for LS1043
    
    Signed-off-by: Sachin Saxena <sachin.saxena@nxp.com>
    Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>

--- lib/librte_mempool/rte_mempool_ops_default.c
+++ lib/librte_mempool/rte_mempool_ops_default.c
@@ -2,6 +2,7 @@
  * Copyright(c) 2016 Intel Corporation.
  * Copyright(c) 2016 6WIND S.A.
  * Copyright(c) 2018 Solarflare Communications Inc.
+ * Copyright 2019 NXP
  */
 
 #include <rte_mempool.h>
@@ -15,6 +16,14 @@ rte_mempool_op_calc_mem_size_helper(const struct rte_mempool *mp,
 	size_t total_elt_sz;
 	size_t obj_per_page, pg_sz, objs_in_last_page;
 	size_t mem_size;
+
+#ifdef RTE_LIBRTE_DPAA_ERRATA_LS1043_A010022
+	/* Reserve more memory as we need to align buffers to 4K boundary.
+	 * This change does not change the number of objects allocated.
+	 */
+	if (mp->flags & MEMPOOL_F_1043_MBUF)
+		obj_num += LS1043_MAX_MEMZONES;
+#endif
 
 	total_elt_sz = mp->header_size + mp->elt_size + mp->trailer_size;
 	if (total_elt_sz == 0) {
@@ -99,6 +108,9 @@ rte_mempool_op_populate_helper(struct rte_mempool *mp, unsigned int flags,
 	unsigned int i;
 	void *obj;
 	int ret;
+#ifdef RTE_LIBRTE_DPAA_ERRATA_LS1043_A010022
+	int idx = 0, change = LS1043_OFFSET_CHANGE_IDX;
+#endif
 
 	ret = rte_mempool_get_page_size(mp, &pg_sz);
 	if (ret < 0)
@@ -123,6 +135,29 @@ rte_mempool_op_populate_helper(struct rte_mempool *mp, unsigned int flags,
 		if (off + total_elt_sz > len)
 			break;
 
+#ifdef RTE_LIBRTE_DPAA_ERRATA_LS1043_A010022
+	/* Due to A010022 hardware errata on LS1043, buf size is kept 4K
+	 * (including metadata). This size is completely divisible by our L1
+	 * cache size (32K) which leads to cache collisions of buffer metadata
+	 * (mbuf) and performance drop. To minimize these cache collisions,
+	 * offset of buffer is changed after an interval of 8 and value is
+	 * reversed after 64 buffer.
+	 */
+		if (mp->flags & MEMPOOL_F_1043_MBUF) {
+			if (off == 0)
+				off = ((uintptr_t)vaddr % total_elt_sz) ?
+					total_elt_sz - ((uintptr_t)vaddr %
+					total_elt_sz) : 0;
+
+			if (idx == LS1043_OFFSET_CHANGE_IDX) {
+				change = -change;
+				idx = 0;
+			}
+			if (idx % LS1043_MAX_BUF_IN_CACHE == 0)
+				off += change;
+			idx++;
+		}
+#endif
 		off += mp->header_size;
 		obj = va + off;
 		obj_cb(mp, obj_cb_arg, obj,
