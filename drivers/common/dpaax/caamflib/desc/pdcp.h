081e6307bbbc41e3b567f6943e98767ae698c57c
Akhil Goyal <akhil.goyal@nxp.com>
2019-12-10 18:20:33 +0530

    common/dpaax/caamflib: remove unnecessary jump for few PDCP cases
    In case of LX2160, PROTOCOL command can be used in some of the PDCP
    cases, in those the jump command prior to KEY command may not be
    required.
    
    The issue observed due to these JUMP command on LX2160 is that,
    the CAAM gets stuck and the processing never get completed. The
    system becomes unusable.
    
    Signed-off-by: Akhil Goyal <akhil.goyal@nxp.com>
    DPDK-2106

--- drivers/common/dpaax/caamflib/desc/pdcp.h
+++ drivers/common/dpaax/caamflib/desc/pdcp.h
@@ -1844,17 +1844,13 @@ pdcp_insert_cplane_snow_zuc_op(struct program *p,
 		return -ENOTSUP;
 	}
 
-	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF | BOTH);
-	KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
-	    cipherdata->keylen, INLINE_KEY(cipherdata));
-	KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
-	    INLINE_KEY(authdata));
-
-	SET_LABEL(p, keyjump);
-
 	if ((rta_sec_era >= RTA_SEC_ERA_8 && sn_size != PDCP_SN_SIZE_18) ||
 		(rta_sec_era == RTA_SEC_ERA_10)) {
 		int pclid;
+		KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
+		    cipherdata->keylen, INLINE_KEY(cipherdata));
+		KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
+		    INLINE_KEY(authdata));
 
 		if (sn_size == PDCP_SN_SIZE_5)
 			pclid = OP_PCLID_LTE_PDCP_CTRL_MIXED;
@@ -1888,6 +1884,13 @@ pdcp_insert_cplane_snow_zuc_op(struct program *p,
 
 	}
 
+	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF | BOTH);
+	KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
+	    cipherdata->keylen, INLINE_KEY(cipherdata));
+	KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
+	    INLINE_KEY(authdata));
+
+	SET_LABEL(p, keyjump);
 	SEQLOAD(p, MATH0, offset, length, 0);
 	JUMP(p, 1, LOCAL_JUMP, ALL_TRUE, CALM);
 	MOVEB(p, MATH0, offset, IFIFOAB2, 0, length, IMMED);
@@ -1965,15 +1968,13 @@ pdcp_insert_cplane_aes_zuc_op(struct program *p,
 		return -ENOTSUP;
 	}
 
-	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF | BOTH);
-	KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
-	    cipherdata->keylen, INLINE_KEY(cipherdata));
-	KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
-	    INLINE_KEY(authdata));
-
 	if ((rta_sec_era >= RTA_SEC_ERA_8 && sn_size != PDCP_SN_SIZE_18) ||
 		(rta_sec_era == RTA_SEC_ERA_10)) {
 		int pclid;
+		KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
+		    cipherdata->keylen, INLINE_KEY(cipherdata));
+		KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
+		    INLINE_KEY(authdata));
 
 		if (sn_size == PDCP_SN_SIZE_5)
 			pclid = OP_PCLID_LTE_PDCP_CTRL_MIXED;
@@ -2007,8 +2008,14 @@ pdcp_insert_cplane_aes_zuc_op(struct program *p,
 		return -ENOTSUP;
 
 	}
+	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF | BOTH);
+	KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
+	    cipherdata->keylen, INLINE_KEY(cipherdata));
+	KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
+	    INLINE_KEY(authdata));
 
 	SET_LABEL(p, keyjump);
+
 	SEQLOAD(p, MATH0, offset, length, 0);
 	JUMP(p, 1, LOCAL_JUMP, ALL_TRUE, CALM);
 	MOVEB(p, MATH0, offset, IFIFOAB2, 0, length, IMMED);
@@ -2089,15 +2096,13 @@ pdcp_insert_cplane_zuc_snow_op(struct program *p,
 		return -ENOTSUP;
 	}
 
-	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF | BOTH);
-	KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
-	    cipherdata->keylen, INLINE_KEY(cipherdata));
-	KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
-	    INLINE_KEY(authdata));
-
 	if ((rta_sec_era >= RTA_SEC_ERA_8 && sn_size != PDCP_SN_SIZE_18) ||
 		(rta_sec_era == RTA_SEC_ERA_10)) {
 		int pclid;
+		KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
+		    cipherdata->keylen, INLINE_KEY(cipherdata));
+		KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
+		    INLINE_KEY(authdata));
 
 		if (sn_size == PDCP_SN_SIZE_5)
 			pclid = OP_PCLID_LTE_PDCP_CTRL_MIXED;
@@ -2131,6 +2136,12 @@ pdcp_insert_cplane_zuc_snow_op(struct program *p,
 		return -ENOTSUP;
 
 	}
+	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF | BOTH);
+	KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
+	    cipherdata->keylen, INLINE_KEY(cipherdata));
+	KEY(p, KEY2, authdata->key_enc_flags, authdata->key, authdata->keylen,
+	    INLINE_KEY(authdata));
+
 	SET_LABEL(p, keyjump);
 	SEQLOAD(p, MATH0, offset, length, 0);
 	JUMP(p, 1, LOCAL_JUMP, ALL_TRUE, CALM);
