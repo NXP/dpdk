df2c6c7503cd1ede8e2ebf655e63b63450aa5d84
Xiao Wang <xiao.w.wang@intel.com>
2020-01-20 22:06:56 -0500

    examples/l3fwd-power: fix a typo
    [ upstream commit dd22d31721b38dd49b7eb7b6c7a3802c9c3cb02e ]
    
    Fixes: aee3bc79cc34 ("examples/l3fwd-power: enable one-shot Rx interrupt and polling switch")
    
    Signed-off-by: Xiao Wang <xiao.w.wang@intel.com>
    Acked-by: David Hunt <david.hunt@intel.com>

--- examples/l3fwd-power/main.c
+++ examples/l3fwd-power/main.c
@@ -1336,7 +1336,7 @@ start_rx:
 				 */
 				rte_delay_us(lcore_idle_hint);
 			else {
-				/* suspend until rx interrupt trigges */
+				/* suspend until rx interrupt triggers */
 				if (intr_en) {
 					turn_on_intr(qconf);
 					sleep_until_rx_interrupt(
