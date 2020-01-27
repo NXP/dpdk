f390adbe4210ee65f020b6e8044aba50a9d57c91
Gagandeep Singh <g.singh@nxp.com>
2020-01-27 11:18:47 +0530

    PVT: nxp: fix dpdk configure scrpt for ls1028
    this patch fixes the networking for ls1028ardb
    board.
    
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>
    DPDK-2210

--- nxp/enetc/dpdk_configure_1028ardb.sh
+++ nxp/enetc/dpdk_configure_1028ardb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2019 NXP
+# Copyright 2019-2020 NXP
 #
 # DPDK script to configure 1 switch (swp0) and 1 enetc port
 # for forwarding
@@ -9,55 +9,40 @@
 #
 
 BRIDGE=br0
-MAC_ROOT=bc:8d:bf:7c:5b
-SW_NETNS=swns
-EXEC_SWNS="ip netns exec $SW_NETNS"
-
-# Create bridge namespace
-ip netns add $SW_NETNS
-# Create bridge device in net namespace
-$EXEC_SWNS ip link add name $BRIDGE type bridge
-$EXEC_SWNS ip link set $BRIDGE up
-
-# Configure switch ports
-#  * set MAC address
-#  * bring up interface
-#  * move net device into the bridge net namespace
-#  * set bridge device as master
-swps=($(ls /sys/bus/pci/devices/0000:00:00.5/net/))
-nr=${#swps[@]}
-for (( i=0; i<$nr; i++ ))
-do
-	echo "adding ${swps[$i]} to brigde .."
-	ip link set ${swps[$i]} address $MAC_ROOT:$(echo "${swps[$i]}" | tr -dc '0-9')
-	ip link set ${swps[$i]} netns $SW_NETNS
-	$EXEC_SWNS ip link set ${swps[$i]} master $BRIDGE
-	if [[ "${swps[$i]}" == "swp0" || "${swps[$i]}" == "swp4" ]]
-	then
-		$EXEC_SWNS ip link set ${swps[$i]} up
-	else
-		$EXEC_SWNS ifconfig ${swps[$i]} down
-	fi
-done
-
-# move ENETC port connected to switch CPU port in bridge ns
-enetc3=$(ls /sys/bus/pci/devices/0000:00:00.6/net/)
-ip link set $enetc3 netns $SW_NETNS
-$EXEC_SWNS ifconfig $enetc3 down
-
-# Check configuration
-$EXEC_SWNS bridge link show
 
 # Mount hugetlbfs
 mkdir -p /dev/hugepages
 mount -t hugetlbfs hugetlbfs /dev/hugepages
 echo 256 > /proc/sys/vm/nr_hugepages
 
-# Bind enetc devices to vfio
+# Bind enetc0 device to vfio
 echo vfio-pci > /sys/bus/pci/devices/0000\:00\:00.0/driver_override
 echo 0000:00:00.0 > /sys/bus/pci/drivers/fsl_enetc/unbind
 echo 0000:00:00.0 > /sys/bus/pci/drivers/vfio-pci/bind
 
-echo vfio-pci > /sys/bus/pci/devices/0000\:00\:00.2/driver_override
-echo 0000:00:00.2 > /sys/bus/pci/drivers/fsl_enetc/unbind
-echo 0000:00:00.2 > /sys/bus/pci/drivers/vfio-pci/bind
+swps=($(ls /sys/bus/pci/devices/0000:00:00.5/net/))
+nr=${#swps[@]}
+
+# Create bridge device
+ip link add name $BRIDGE type bridge
+
+# Up the kernel enetc2 port
+enetc2=$(ls /sys/bus/pci/devices/0000:00:00.2/net/)
+ifconfig $enetc2 up
+
+for (( i=0; i<$nr; i++ ))
+do
+	echo "adding ${swps[$i]} to brigde .."
+	ip link set ${swps[$i]} master $BRIDGE
+	if [[ "${swps[$i]}" == "swp0" || "${swps[$i]}" == "swp5" ]]
+	then
+		ifconfig ${swps[$i]} up
+	else
+		ifconfig ${swps[$i]} down
+	fi
+done
+
+echo vfio-pci > /sys/bus/pci/devices/0000\:00\:00.6/driver_override
+echo 0000:00:00.6 > /sys/bus/pci/drivers/fsl_enetc/unbind
+echo 0000:00:00.6 > /sys/bus/pci/drivers/vfio-pci/bind
+ifconfig $BRIDGE up
