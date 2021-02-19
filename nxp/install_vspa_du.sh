248ea7495fcb02403b504fb086f43e3d42081172
Nipun Gupta <nipun.gupta@nxp.com>
2021-02-19 15:49:55 +0530

    nxp: PVT: add script to install VSPA binary for DU
    Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>

---
+++ nxp/install_vspa_du.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2021 NXP
+
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du0.eld
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du1.eld
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du2.eld
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du3.eld
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du4.eld
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du5.eld
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du6.eld
+ln -fs /lib/firmware/geul-vspa-du.eld /lib/firmware/geul-vspa-du7.eld
+sync
