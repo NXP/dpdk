4ec7adb38240ed0c11b60e17e847f90170a847ca
Gagandeep Singh <g.singh@nxp.com>
2021-12-14 15:42:01 +0530

    UP: crypto/dpaa_sec: physically enable QI
    To perform crypto operations on DPAA platform,
    QI interface of HW must be enabled.
    Earlier DPAA crypto driver was dependant on
    kernel for QI enable. Now with this patch
    there is no such dependancy on kernel.
    
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/crypto/dpaa_sec/dpaa_sec.h
+++ drivers/crypto/dpaa_sec/dpaa_sec.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  *
- *   Copyright 2016-2021,2023 NXP
+ *   Copyright 2016-2024 NXP
  *
  */
 
@@ -10,6 +10,12 @@
 #define CRYPTODEV_NAME_DPAA_SEC_PMD	crypto_dpaa_sec
 /**< NXP DPAA - SEC PMD device name */
 
+#define SEC_BASE_ADDR		0x1700000
+#define MAP_SIZE		0x100000
+#define BLOCK_OFFSET		0x10000
+#define CMD_REG			0x4
+#define QICTL_DQEN		0x01
+#define QI_BLOCK_NUMBER		7
 #define MAX_DPAA_CORES		4
 #define NUM_POOL_CHANNELS	4
 #define DPAA_SEC_BURST		7
