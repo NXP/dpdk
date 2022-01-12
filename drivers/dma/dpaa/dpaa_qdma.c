642e435e0e9f857d373463154f5fff0a15c72aa4
Gagandeep Singh <g.singh@nxp.com>
2022-01-12 14:25:55 +0530

    dma/dpaa: support multi channels
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/dma/dpaa/dpaa_qdma.c
+++ drivers/dma/dpaa/dpaa_qdma.c
@@ -648,8 +648,8 @@ fsl_qdma_alloc_chan_resources(struct fsl_qdma_chan *fsl_chan)
 	}
 
 finally:
-	return fsl_qdma->desc_allocated++;
-
+	fsl_qdma->desc_allocated++;
+	return 0;
 exit:
 	return -ENOMEM;
 }
@@ -670,7 +670,7 @@ dpaa_info_get(const struct rte_dma_dev *dev, struct rte_dma_info *dev_info,
 			     RTE_DMA_CAPA_DEV_TO_MEM |
 			     RTE_DMA_CAPA_SILENT |
 			     RTE_DMA_CAPA_OPS_COPY;
-	dev_info->max_vchans = 1;
+	dev_info->max_vchans = 4;
 	dev_info->max_desc = DPAADMA_MAX_DESC;
 	dev_info->min_desc = DPAADMA_MIN_DESC;
 
