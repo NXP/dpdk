390bbe6e5403f6c0eb2d999950bae3b7e0cfeaec
Vanshika Shukla <vanshika.shukla@nxp.com>
2022-05-11 16:01:33 +0530

    net/dpaa2: fix TX Confirmation logic
    This patch introduces DPAA2_TX_CONF compile time
    flag to fix TX confirmation logic. This flag will
    avoid timestamp calculations but will enable Tx
    confirmation when required.
    
    Signed-off-by: Vanshika Shukla <vanshika.shukla@nxp.com>
    DPDK-3517

--- drivers/net/dpaa2/dpaa2_rxtx.c
+++ drivers/net/dpaa2/dpaa2_rxtx.c
@@ -1229,13 +1229,17 @@ dpaa2_dev_tx(void *queue, struct rte_mbuf **bufs, uint16_t nb_pkts)
 	DPAA2_PMD_DP_DEBUG("===> eth_data =%p, fqid =%d\n",
 			eth_data, dpaa2_q->fqid);
 
+#if (!defined DPAA2_TX_CONF) || (!defined RTE_LIBRTE_IEEE1588)
+	if (priv->flags & DPAA2_TX_CONF_ENABLE)
+#endif
+		dpaa2_dev_tx_conf(dpaa2_q->tx_conf_queue);
+
 #ifdef RTE_LIBRTE_IEEE1588
 	/* IEEE1588 driver need pointer to tx confirmation queue
 	 * corresponding to last packet transmitted for reading
 	 * the timestamp
 	 */
 	priv->next_tx_conf_queue = dpaa2_q->tx_conf_queue;
-	dpaa2_dev_tx_conf(dpaa2_q->tx_conf_queue);
 #endif
 
 	/*Prepare enqueue descriptor*/
