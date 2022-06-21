5edf4530a7aac4bedfee41ce80712b1ee4830069
Ciara Power <ciara.power@intel.com>
2022-06-21 11:41:32 +0000

    test/crypto: fix cipher offset for ZUC
    [ upstream commit 7a0f8fe76bd03dc8b61965040a66409996afa717 ]
    
    The cipher offset in bits was not being used in ZUC encryption test
    functions when creating the operation, it was hardcoded to 0.
    This is fixed to use the offset from the test vector as intended.
    
    Fixes: fd01a9be38d5 ("test/crypto: move IV to crypto op private data")
    
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Acked-by: Kai Ji <kai.ji@intel.com>

--- app/test/test_cryptodev.c
+++ app/test/test_cryptodev.c
@@ -6191,7 +6191,7 @@ test_zuc_encryption(const struct wireless_test_data *tdata)
 	retval = create_wireless_algo_cipher_operation(tdata->cipher_iv.data,
 					tdata->cipher_iv.len,
 					tdata->plaintext.len,
-					0);
+					tdata->validCipherOffsetInBits.len);
 	if (retval < 0)
 		return retval;
 
@@ -6286,7 +6286,7 @@ test_zuc_encryption_sgl(const struct wireless_test_data *tdata)
 	/* Create ZUC operation */
 	retval = create_wireless_algo_cipher_operation(tdata->cipher_iv.data,
 			tdata->cipher_iv.len, tdata->plaintext.len,
-			0);
+			tdata->validCipherOffsetInBits.len);
 	if (retval < 0)
 		return retval;
 
