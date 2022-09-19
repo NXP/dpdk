f68a2839bca91742af7aa38674309bc075025cce
Vanshika Shukla <vanshika.shukla@nxp.com>
2022-09-19 12:21:40 +0530

    net/dpaa2: Add support for DPDMUX_METHOD_C_VLAN_MAC
    This patch adds the support for DPDMUX_METHOD_C_VLAN_MAC method
    which implements DPDMUX based on C-VLAN and MAC address.
    
    Signed-off-by: Vanshika Shukla <vanshika.shukla@nxp.com>
    DPDK-3660

--- drivers/net/dpaa2/rte_pmd_dpaa2.h
+++ drivers/net/dpaa2/rte_pmd_dpaa2.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2018-2021 NXP
+ * Copyright 2018-2022 NXP
  */
 
 #ifndef _RTE_PMD_DPAA2_H
@@ -31,6 +31,13 @@ struct rte_flow *
 rte_pmd_dpaa2_mux_flow_create(uint32_t dpdmux_id,
 			      struct rte_flow_item *pattern[],
 			      struct rte_flow_action *actions[]);
+
+
+struct dpdmux_l2_rule*
+rte_pmd_dpaa2_mux_flow_l2(uint32_t dpdmux_id,
+			  uint8_t mac_addr[6],
+			  uint16_t vlan_id,
+			  int dest_if);
 
 /**
  * @warning
