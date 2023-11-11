e4bf277fe3ddd22abb1b70951c96a890d5d9892a
Jun Yang <jun.yang@nxp.com>
2023-11-11 13:34:57 -0800

    examples/qdma_demo: Multiple formats support
    Support shaort format, long format and SG format and
    specify PCIe space test size.
    
    --format=<short, long, sg>
    --pci_size=<hex size>
    
    Signed-off-by: Jun Yang <jun.yang@nxp.com>

--- examples/qdma_demo/qdma_demo.h
+++ examples/qdma_demo/qdma_demo.h
@@ -12,11 +12,15 @@
 #define PCI_TO_MEM 3
 #define MEM_TO_MEM 4
 
-#define TEST_PACKETS_NUM (g_packet_num)
-#define TEST_PCI_DEFAULT_SIZE (32 * 1024)
-#define TEST_PACKET_SIZE (g_packet_size)
-
 #define QDMA_DEMO_MAX_VQS 2048
+#define QDMA_DEMO_VQ_MAX_SIZE 1024
+
+struct qdma_demo_vq_format {
+	const char *name;
+	const char *help;
+	uint32_t vq_format;
+};
+#define VQ_FORMAT_NAME_SIZE 30
 
 struct qdma_test_case {
 	const char *name;
@@ -29,10 +33,11 @@ struct qdma_test_case {
 #define ARG_TEST_ID (1 << 2)
 #define ARG_LATENCY (1 << 3)
 #define ARG_MEMCPY (1 << 4)
-#define ARG_SCATTER_GATHER (1 << 5)
-#define ARG_BURST (1 << 6)
-#define ARG_NUM (1 << 7)
-#define ARG_VALIDATE (1 << 8)
-#define ARG_SEG_IOVA (1 << 9)
+#define ARG_BURST (1 << 5)
+#define ARG_NUM (1 << 6)
+#define ARG_VALIDATE (1 << 7)
+#define ARG_SEG_IOVA (1 << 8)
+#define ARG_VQ_FORMAT (1 << 9)
+#define ARG_PCI_SIZE (1 << 10)
 
 #endif
