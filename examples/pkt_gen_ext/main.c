c05cfb0379e1c30a0d7692bdc355a1b5aaffc2d4
Rohit Raj <rohit.raj@nxp.com>
2023-11-15 09:04:20 +0530

    pkt_gen_ext: Fix mbuf allocation failure
    Fix mbuf allocation failure in case of link down
    Also stop Rx/Tx if link down is detected.
    
    Signed-off-by: Rohit Raj <rohit.raj@nxp.com>
    DPDK-4022

--- examples/pkt_gen_ext/main.c
+++ examples/pkt_gen_ext/main.c
@@ -34,6 +34,8 @@ const unsigned char OutFrame[] = {
 		 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
 		 0x0B, 0x0C, 0xAE, 0xFE, 0x0F, 0x10};
 
+static bool is_link_up[RTE_MAX_ETHPORTS];
+
 static void
 set_mbuf_data(struct rte_mbuf *mbuf, const unsigned char *data,
 		   const unsigned int data_len)
@@ -46,10 +48,49 @@ set_mbuf_data(struct rte_mbuf *mbuf, const unsigned char *data,
 	mbuf->next = NULL;
 }
 
+#define CHECK_INTERVAL 100	/* 100ms */
+#define MAX_CHECK_TIME 90	/* 9s (90 * 100ms) in total */
+
+static int
+lsi_event_callback(uint16_t port_id, enum rte_eth_event_type type, void *param,
+		   void *ret_param)
+{
+	struct rte_eth_link link;
+	int ret;
+	char link_status_text[RTE_ETH_LINK_MAX_STR_LEN];
+
+	RTE_SET_USED(type);
+	RTE_SET_USED(param);
+	RTE_SET_USED(ret_param);
+
+	ret = rte_eth_link_get_nowait(port_id, &link);
+	if (ret < 0) {
+		printf("Failed link get on port %d: %s\n", port_id,
+		       rte_strerror(-ret));
+		return ret;
+	}
+
+	ret = rte_eth_link_to_str(link_status_text, sizeof(link_status_text), &link);
+	if (ret < 0) {
+		printf("Failed link to str on port %d: %s\n", port_id,
+		       rte_strerror(-ret));
+		return ret;
+	}
+	printf("Port %d %s\n\n", port_id, link_status_text);
+
+	is_link_up[port_id] = link.link_status;
+
+	return 0;
+}
+
 static int
 open_port(uint16_t port_id)
 {
 	int rez = 0;
+	uint8_t count;
+	struct rte_eth_link link = {0};
+	uint16_t hw_rx_queue_size = 1024;
+	uint16_t hw_tx_queue_size = 1024;
 
 	if (port_id >= rte_eth_dev_count_avail()) {
 		printf("Devices:%d\n", rte_eth_dev_count_avail());
@@ -67,6 +108,8 @@ open_port(uint16_t port_id)
 	port_conf.rx_adv_conf.rss_conf.rss_key = NULL;
 	port_conf.rx_adv_conf.rss_conf.rss_key_len = 0;
 
+	port_conf.intr_conf.lsc = 1;
+
 	/* Configure the Ethernet device. */
 	rez = rte_eth_dev_configure(port_id, 1, 1, &port_conf);
 	if (rez != 0) {
@@ -74,15 +117,15 @@ open_port(uint16_t port_id)
 		return -2;
 	}
 
-	uint16_t hw_rx_queue_size = 1024;
-	uint16_t hw_tx_queue_size = 1024;
-
 	rez = rte_eth_dev_adjust_nb_rx_tx_desc(port_id, &hw_rx_queue_size,
 						&hw_tx_queue_size);
 	if (rez != 0) {
 		printf("%s:%d: E_FAIL\n", __func__, __LINE__);
 		return -3;
 	}
+
+	rte_eth_dev_callback_register(port_id, RTE_ETH_EVENT_INTR_LSC,
+				      lsi_event_callback, NULL);
 
 	if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
 		l2fwd_pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", 8192,
@@ -101,17 +144,17 @@ open_port(uint16_t port_id)
 	}
 
 	/* Allocate and set up RX queue */
-	rez = rte_eth_rx_queue_setup(port_id, 0, 1024,
-			rte_eth_dev_socket_id(port_id), NULL,
-			l2fwd_pktmbuf_pool);
+	rez = rte_eth_rx_queue_setup(port_id, 0, hw_rx_queue_size,
+				     rte_eth_dev_socket_id(port_id), NULL,
+				     l2fwd_pktmbuf_pool);
 	if (rez < 0) {
 		printf("%s:%d: E_FAIL\n", __func__, __LINE__);
 		return -5;
 	}
 
 	/* Allocate and set up 1 TX queue per Ethernet port_id. */
-	rez = rte_eth_tx_queue_setup(port_id, 0, 1024,
-			rte_eth_dev_socket_id(port_id), NULL);
+	rez = rte_eth_tx_queue_setup(port_id, 0, hw_tx_queue_size,
+				     rte_eth_dev_socket_id(port_id), NULL);
 	if (rez < 0) {
 		printf("%s:%d: E_FAIL\n", __func__, __LINE__);
 		return -6;
@@ -126,8 +169,40 @@ open_port(uint16_t port_id)
 		return -7;
 	}
 
+	printf("\nChecking link status");
+	fflush(stdout);
+
+	for (count = 0; count <= MAX_CHECK_TIME; count++) {
+		if (!link.link_status) {
+			rez = rte_eth_link_get_nowait(port_id, &link);
+			if (rez < 0) {
+				printf("%s:%d: E_FAIL\n", __func__, __LINE__);
+				return -8;
+			}
+			printf(".");
+			fflush(stdout);
+			rte_delay_ms(CHECK_INTERVAL);
+		} else
+			break;
+	}
+	is_link_up[port_id] = link.link_status;
+
+	if (!is_link_up[port_id]) {
+		printf("%s:%d: Port %d Link Down\n", __func__, __LINE__,
+		       port_id);
+		return -9;
+	}
+
+	printf("Port%d Link Up. Speed %u Mbps - %s\n", port_id, link.link_speed,
+	       (link.link_duplex == RTE_ETH_LINK_FULL_DUPLEX) ? ("full-duplex")
+	       : ("half-duplex"));
+
 	/* Enable RX in promiscuous mode for the Ethernet device */
-	rte_eth_promiscuous_enable(port_id);
+	rez = rte_eth_promiscuous_enable(port_id);
+	if (rez < 0) {
+		printf("%s:%d: E_FAIL\n", __func__, __LINE__);
+		return -10;
+	}
 
 	return 0;
 }
@@ -189,18 +264,32 @@ int main(__rte_unused int argc, char *argv[])
 		struct rte_mbuf_ext_shared_info *ret_shinfo = NULL;
 		rte_iova_t buf_iova;
 
+		if (!is_link_up[DPDK_PORT_ID]) {
+			printf("Error: Link is Down. Stopping Rx/Tx...\n");
+			break;
+		}
+
 		m = rte_pktmbuf_alloc(l2fwd_pktmbuf_pool);
-		if (m == NULL)
+		if (m == NULL) {
 			printf("mbuf failed\n");
+			return -ENOMEM;
+		}
 		set_mbuf_data(m, OutFrame, sizeof(OutFrame));
 		hdr = rte_pktmbuf_alloc(l2fwd_pktmbuf_pool);
-		if (hdr == NULL)
+		if (hdr == NULL) {
 			printf("%s: mbuf allocation failed!\n", __func__);
+			rte_pktmbuf_free(m);
+			return -ENOMEM;
+		}
 		ext_buf_addr = rte_malloc("External buffer", buf_len,
 				RTE_CACHE_LINE_SIZE);
-		if (ext_buf_addr == NULL)
+		if (ext_buf_addr == NULL) {
 			printf("%s: External buffer allocation failed\n",
 					__func__);
+			rte_pktmbuf_free(hdr);
+			rte_pktmbuf_free(m);
+			return -ENOMEM;
+		}
 
 		ret_shinfo = rte_pktmbuf_ext_shinfo_init_helper(ext_buf_addr,
 				&buf_len,
@@ -219,8 +308,15 @@ int main(__rte_unused int argc, char *argv[])
 		m->next = hdr;
 		m->nb_segs += hdr->nb_segs;
 		m->pkt_len = (uint16_t)(hdr->data_len + m->pkt_len);
+
 		/*XXX:  instead of above calc, use rte_pktmbuf_chain here */
 		ret = rte_eth_tx_burst(DPDK_PORT_ID, 0, &m, 1);
+		if (ret == 0) {
+			rte_free(ext_buf_addr);
+			rte_pktmbuf_free(hdr);
+			rte_pktmbuf_free(m);
+		}
+
 		sent_pkts += ret;
 	}
 	printf("Packets sent. Count=%"PRIu64" and callback count = %d\n", sent_pkts,
