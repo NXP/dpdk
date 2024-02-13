e4a87d2e2751a90188d9877b72b21d805d53f45e
Hemant Agrawal <hemant.agrawal@nxp.com>
2024-02-13 15:20:42 +0530

    update debug_dump.sh script for i.mx platform
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- nxp/debug_dump.sh
+++ nxp/debug_dump.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -i
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2018-2023 NXP
+# Copyright 2018-2024 NXP
 
 # tunable parameters
 
-logoutput="dpdk_debug_"
+logoutput="nxp_debug_"
 logoutput="${logoutput}_"`date +%d%m%Y_%H%M%S`".txt"
 silent=0
 # Some colors
@@ -45,10 +45,19 @@ function system() {
 	mycmd "cat /proc/cmdline"
 	print "*************** CPU Architecture*******"
 	mycmd "lscpu"
+	print "*************** SoC Details *******"
+	mycmd "cat /sys/bus/soc/devices/soc0/soc_id"
+	mycmd "ls /sys/bus/soc/devices/soc0/"
+	mycmd "cat /sys/bus/soc/devices/soc0/*"
+
 	print "*************** PCI details *******"
 	mycmd "lspci -v"
 	print "*************** Block Details *******"
+	mycmd "df -k"
 	mycmd "lsblk"
+	mycmd "journalctl -b -u rpm-ostreed"
+	mycmd "cat /proc/mounts"
+	mycmd "cat /proc/swaps"
 	print "*************** Loaded modules *******"
 	mycmd "lsmod"
 	print "*************** CPU INFO *******"
@@ -56,14 +65,17 @@ function system() {
 	print "*************** MEMINFO *******"
 	mycmd "cat /proc/meminfo"
 	mycmd "cat /proc/iomem"
+	mycmd "free -m"
 	print "*************** interrupts *******"
 	mycmd "cat /proc/interrupts"
 	print "*************** misc *******"
+	mycmd "uptime"
 	mycmd "cat /proc/devices"
 	mycmd "cat /proc/execdomains"
 	mycmd "cat /proc/buddyinfo"
 	mycmd "cat /proc/misc"
-
+	mycmd "cat /proc/stat"
+	mycmd "cat /sys/class/rtc/rtc0/time"
 	print "*************** network interfaces"
 	mycmd "ifconfig -a"
 	print "*************** Distro information"
@@ -71,15 +83,20 @@ function system() {
 	mycmd "cat /proc/version"
 	mycmd "lsb_release -a"
 
+	print "*************** thermal *******"
+	mycmd "cat /sys/class/thermal/thermal_zone0/temp"
+	mycmd "cat /sys/class/thermal/thermal_zone1/temp"
+
 	print "*************** Env"
 	mycmd "env"
 
 	print "*************** Performance Settings"
 	local gov_set=`cat /sys/devices/system/cpu/cpufreq/policy0/scaling_governor`
 	print "Performance governor set to: ${gov_set}"
+	local cur_freq=`cat /sys/devices/system/cpu/cpufreq/policy0/scaling_cur_freq`
 	local max_freq=`cat /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq`
 	local min_freq=`cat /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq`
-	print "Current Max CPU freq is ${max_freq} and Min is ${min_freq}"
+	print "Current ${cur_freq} Max CPU freq is ${max_freq} and Min is ${min_freq}"
 	local rt_us=`cat /proc/sys/kernel/sched_rt_runtime_us`
 	print "Value for Userspace non-RT slice: ${rt_us}"
 }
@@ -160,6 +177,39 @@ function dpaa2_info() {
 	done
 }
 
+function imx_info() {
+	print "*************** IMX Ethernet"
+	mycmd "cat /proc/device-tree/aliases/ethernet0"
+	mycmd "cat /proc/device-tree/aliases/ethernet1"
+	mycmd "cat /proc/device-tree/aliases/ethernet2"
+	mycmd "cat /proc/device-tree/soc@0/bus@30be0000/ethernet@30bf0000/compatible"
+	mycmd "cat /proc/device-tree/soc@0/bus@30800000/ethernet@30bf0000/compatible"
+	mycmd "cat /proc/device-tree/soc@0/bus@30800000/ethernet@30be0000/compatible"
+	mycmd "cat /proc/device-tree/reserved-memory/"
+
+	print "*************** IMX Performance Settings"
+        local gov_set=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`
+        print "Performance governor set to: ${gov_set}"
+	local cur_freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq`
+        local max_freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq`
+        local min_freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq`
+        print "Current ${cur_freq} Max CPU freq is ${max_freq} and Min is ${min_freq}"
+}
+
+function networking_stats() {
+	print "*************** Networking Stats"
+	mycmd "cat /proc/net/arp
+	mycmd "cat /proc/net/dev
+	mycmd "cat /proc/net/netstat
+	mycmd "cat /proc/net/sockstat
+	mycmd "cat /proc/net/wireless
+	mycmd "cat /proc/net/netlink
+	mycmd "cat /proc/net/rt_cache
+	mycmd "cat /proc/net/route
+	mycmd "cat /proc/net/ipv6_route
+	mycmd "cat /proc/crypto
+}
+
 function usage() {
 	echo "Usage: $0 [-o <logfile>]"
 	echo "           -o Output file. if not specified and randomly generated"
@@ -173,7 +223,7 @@ function usage() {
 function board_config() {
 	if [ -e /sys/firmware/devicetree/base/compatible ]
 	then
-	board_type=`grep -ao 'imx8m\|imx9\|1028\|1046\|1043\|1088\|2088\|2160\|2162' /sys/firmware/devicetree/base/compatible | head -1`
+	board_type=`grep -ao 'imx8\|imx95\|imx9\|1028\|1046\|1043\|1088\|2088\|2160\|2162' /sys/firmware/devicetree/base/compatible | head -1`
 	fi
 
 	echo "Board type ${board_type} detected."
@@ -183,25 +233,32 @@ function board_config() {
 	if [[ $board_type == "1088" || $board_type == "2088" || $board_type == "2160" || $board_type == "2162" ]]
 	then
 		platform="dpaa2"
-		echo "platform detected is ${platform}"
+		echo "networking platform detected is ${platform}"
 		dpaa2_info
 	fi
 	if [[ $board_type == "1043" || $board_type == "1046" ]]
 	then
 		platform="dpaa"
-		echo "platform detected is ${platform}"
+		echo "networking platform detected is ${platform}"
 		dpaa_info
 	fi
 
 	if [[ $board_type == "1028" ]]
 	then
 		platform="enetc"
-		echo "platform detected is ${platform}"
+		echo "networking platform detected is ${platform}"
 	fi
-	if [[ $board_type == "imx8m" || $board_type == "imx9" ]]
+	if [[ $board_type == "imx95"  ]]
 	then
-		platform="fec-enet"
-		echo "platform detected is ${platform}"
+		platform="enetc"
+		echo "networking platform detected is ${platform}"
+		imx_info
+	fi
+	if [[ $board_type == "imx8" || $board_type == "imx9" ]]
+	then
+		platform="fec-enet or enet-qos"
+		echo "networking platform detected is ${platform}"
+		imx_info
 	fi
 }
 
@@ -218,6 +275,8 @@ function dump_configuration() {
 	mycmd "zcat /proc/config.gz"
 	print "===============Device Tree =================="
 	devicetree
+	print "===============Networking Info=================="
+	#networking_stats
 }
 
 while getopts ":o:h" o; do
