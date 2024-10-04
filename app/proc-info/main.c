d7e77f3f6d97e7a222a2a6416de2ba3c310c2723
Fidaullah Noonari <fidaullah.noonari@emumba.com>
2024-10-03 19:48:29 -0700

    app/procinfo: fix leak on exit
    [ upstream commit 8a171e52ed8b26f768ced79a22286914ebd30180 ]
    
    When app is launched with -m proc-info exit without
    rte_eal_cleanup() causing memory leakage. This commit resolves the
    memory leakage issue and closes app properly.
    
    Bugzilla ID: 898
    Fixes: 67684d1e87b6 ("app/procinfo: call EAL cleanup before exit")
    Fixes: 674bb3906931 ("app/procinfo: display eventdev xstats")
    
    Signed-off-by: Fidaullah Noonari <fidaullah.noonari@emumba.com>
    Acked-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Chengwen Feng <fengchengwen@huawei.com>

--- app/proc-info/main.c
+++ app/proc-info/main.c
@@ -1798,7 +1798,7 @@ main(int argc, char **argv)
 
 	if (mem_info) {
 		meminfo_display();
-		return 0;
+		goto cleanup;
 	}
 
 	nb_ports = rte_eth_dev_count_avail();
@@ -1880,6 +1880,7 @@ main(int argc, char **argv)
 	RTE_ETH_FOREACH_DEV(i)
 		rte_eth_dev_close(i);
 
+cleanup:
 	ret = rte_eal_cleanup();
 	if (ret)
 		printf("Error from rte_eal_cleanup(), %d\n", ret);
