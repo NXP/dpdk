25e54a672e05b1e46c3480b586677c747fde8e75
Vanshika Shukla <vanshika.shukla@nxp.com>
2024-12-23 13:47:05 +0530

    net/enetc: Fix Resource leak Coverity issue
    This patch fixes coverity issue: Resource leak reported
    by our internal coverity tool.
    
    CID: 41770268
    
    Signed-off-by: Vanshika Shukla <vanshika.shukla@nxp.com>
    DPDK-4419

--- drivers/net/enetc/enetc4_ethdev.c
+++ drivers/net/enetc/enetc4_ethdev.c
@@ -36,12 +36,18 @@ static int parse_txq_prior(const char *key __rte_unused, const char *value,
 	uint32_t i = 0;
 
 	hw->txq_prior = rte_zmalloc(NULL, hw->max_tx_queues * sizeof(uint32_t), 0);
+	if (!hw->txq_prior) {
+		free(input_str);
+		return -1;
+	}
+
 	str = strtok(input_str, "|");
 	while (str != NULL && i < hw->max_tx_queues) {
 		hw->txq_prior[i++] = atoi(str);
 		str = strtok(NULL, "|");
 	}
 
+	free(input_str);
 	return 0;
 }
 
