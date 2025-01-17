cdf3726e6ae4e1b8e04ccea817657ac813ad899a
Vanshika Shukla <vanshika.shukla@nxp.com>
2025-01-17 15:07:16 +0530

    event/dpaa2: Fix coverity issue
    Fix Uninitialized scalar variable CID: 323497 issue
    reported by external coverity tool.
    
    Signed-off-by: Vanshika Shukla <vanshika.shukla@nxp.com>

--- drivers/event/dpaa2/dpaa2_eventdev.c
+++ drivers/event/dpaa2/dpaa2_eventdev.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2017,2019-2024 NXP
+ * Copyright 2017,2019-2025 NXP
  */
 
 #include <assert.h>
@@ -991,6 +991,7 @@ dpaa2_eventdev_setup_dpci(struct dpaa2_dpci_dev *dpci_dev,
 	rx_queue_cfg.dest_cfg.dest_type = DPCI_DEST_DPCON;
 	rx_queue_cfg.dest_cfg.dest_id = dpcon_dev->dpcon_id;
 	rx_queue_cfg.dest_cfg.priority = DPAA2_EVENT_DEFAULT_DPCI_PRIO;
+	rx_queue_cfg.order_preservation_en = 0;
 
 	dpci_dev->rx_queue[DPAA2_EVENT_DPCI_PARALLEL_QUEUE].cb =
 		dpaa2_eventdev_process_parallel;
