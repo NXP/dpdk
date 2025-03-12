5f567da654c96d1b7b8d9ca2ace65736fad49352
Jun Yang <jun.yang@nxp.com>
2025-03-12 03:37:48 -0700

    net/dpaa2: TX burst to multiple ports
    Send out multiple frames to multiple ports respectively by
    single TX burst.
    
    Signed-off-by: Jun Yang <jun.yang@nxp.com>

--- drivers/net/dpaa2/rte_pmd_dpaa2.h
+++ drivers/net/dpaa2/rte_pmd_dpaa2.h
@@ -151,6 +151,10 @@ rte_pmd_dpaa2_clean_tx_conf(uint32_t eth_id,
 int
 rte_pmd_dpaa2_rx_get_offset(struct rte_mbuf *m,
 	uint8_t *l3_off, uint8_t *l4_off, uint8_t *l5_off);
+uint16_t
+rte_dpaa2_dev_tx_multi_ports(uint16_t port_id[],
+	uint16_t txq_id[], struct rte_mbuf **bufs,
+	uint16_t nb_pkts);
 
 #if defined(RTE_LIBRTE_IEEE1588)
 __rte_experimental
