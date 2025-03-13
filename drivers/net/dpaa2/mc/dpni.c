13dbe943d65363adc388831b87fd9da3ff516aea
Ioana Ciornei <ioana.ciornei@nxp.com>
2025-03-13 17:44:45 +0200

    net/dpaa2: setup the speed_capa based on the actual MAC's capability
    Previously, the speed_capa field from struct rte_eth_dev_info was
    populated with all possible speeds that the MACs on the system could
    sustain. What this meant is that the bitmap of speed capability did not
    reflect what the MAC could do in the current circumstance.
    
    Fix this by using the newly added MC command
    dpni_get_mac_speed_capability() which returns a bitmap of
    enum dpmac_link_speed. Since the MC API is a newly added one, we check
    for the DPNI version to determine is we can use the API, if not the code
    will fallback to the procedure used up until now.
    
    Also, we interogate the MC firmware only at probe time to get the needed
    into and then we store the the information in the private structure to
    use it any time .dev_infos_get() is called.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

--- drivers/net/dpaa2/mc/dpni.c
+++ drivers/net/dpaa2/mc/dpni.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
  *
  * Copyright 2013-2016 Freescale Semiconductor Inc.
- * Copyright 2016-2024 NXP
+ * Copyright 2016-2025 NXP
  *
  */
 #include <fsl_mc_sys.h>
@@ -4172,4 +4172,24 @@ int dpni_get_mac_statistics(struct fsl_mc_io *mc_io, uint32_t cmd_flags, uint16_
 	cmd_params->num_cnt = cpu_to_le32(num_cnt);
 
 	return mc_send_command(mc_io, &cmd);
+}
+
+int dpni_get_mac_speed_capability(struct fsl_mc_io *mc_io, uint32_t cmd_flags, uint16_t token,
+				  uint32_t *speed_cap)
+{
+	struct dpni_rsp_mac_speed_cap *rsp_params;
+	struct mc_command cmd = { 0 };
+	int err;
+
+	cmd.header = mc_encode_cmd_header(DPNI_CMDID_GET_MAC_SPEED_CAPABILITY,
+					  cmd_flags, token);
+
+	err = mc_send_command(mc_io, &cmd);
+	if (err)
+		return err;
+
+	rsp_params = (struct dpni_rsp_mac_speed_cap *)cmd.params;
+	*speed_cap = le32_to_cpu(rsp_params->speed_cap);
+
+	return 0;
 }
