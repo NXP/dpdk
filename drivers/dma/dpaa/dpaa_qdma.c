1586acc6a58cb45a36d612161414c556254c86cd
Vanshika Shukla <vanshika.shukla@nxp.com>
2025-08-26 15:09:09 +0530

    Fix coverity issues
    This patch fixed multiple coverity issues reported.
    
    CID 47881369 Overflowed integer argument
    CID 47881379 Overflowed integer argument
    CID 45772364 Overflowed array index read
    CID 45772264 Resource leak
    
    Signed-off-by: Vanshika Shukla <vanshika.shukla@nxp.com>

--- drivers/dma/dpaa/dpaa_qdma.c
+++ drivers/dma/dpaa/dpaa_qdma.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2021-2024 NXP
+ * Copyright 2021-2025 NXP
  */
 
 #include <bus_dpaa_driver.h>
@@ -827,6 +827,11 @@ eq_sg:
 				break;
 			}
 		}
+	}
+
+	if (num == 0 || num > FSL_QDMA_SG_MAX_ENTRY) {
+		DPAA_QDMA_ERR("Invalid scatter-gather entry count: num=%u\n", num);
+		return -EINVAL;
 	}
 
 	ft->desc_ssge[num - 1].final = 1;
