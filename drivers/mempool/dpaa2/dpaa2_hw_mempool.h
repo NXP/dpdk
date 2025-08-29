a49be0c2dcb864f98944a0413a07ae7ef4a83b6a
Jun Yang <jun.yang@nxp.com>
2025-08-28 19:39:05 -0700

    mempool/dpaa2: Fix prefix issue and remove redundant code
    1) Remove "rte_" prefix from static function name.
    2) Introduce common API to allocate mbuf instead of private function.
    
    Signed-off-by: Jun Yang <jun.yang@nxp.com>

--- drivers/mempool/dpaa2/dpaa2_hw_mempool.h
+++ drivers/mempool/dpaa2/dpaa2_hw_mempool.h
@@ -61,10 +61,6 @@ struct dpaa2_bp_info {
 extern struct dpaa2_bp_info *rte_dpaa2_bpid_info;
 
 __rte_internal
-int rte_dpaa2_mbuf_alloc_bulk(struct rte_mempool *pool,
-		       void **obj_table, unsigned int count);
-
-__rte_internal
 int rte_dpaa2_mpool_get_ops_idx(void);
 
 #endif /* _DPAA2_HW_DPBP_H_ */
