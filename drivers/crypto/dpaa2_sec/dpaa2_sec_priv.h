eee000aa3ef18caaaaab6549d72579543f55cc02
Gagandeep Singh <g.singh@nxp.com>
2026-02-10 15:43:06 +0530

    crypto/dpaa2_sec: add missing ecn capability
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/crypto/dpaa2_sec/dpaa2_sec_priv.h
+++ drivers/crypto/dpaa2_sec/dpaa2_sec_priv.h
@@ -1004,6 +1004,7 @@ static const struct rte_security_capability dpaa2_sec_security_cap[] = {
 				.copy_df = 1,
 				.copy_dscp = 1,
 				.dec_ttl = 1,
+				.ecn = 1,
 				.esn = 1,
 			},
 			.replay_win_sz_max = 1024
@@ -1023,6 +1024,7 @@ static const struct rte_security_capability dpaa2_sec_security_cap[] = {
 				.copy_df = 1,
 				.copy_dscp = 1,
 				.dec_ttl = 1,
+				.ecn = 1,
 				.esn = 1,
 			},
 			.replay_win_sz_max = 1024
