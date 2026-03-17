9b1f0b08130a39b3e61c4ca03c3f946cc9faf6a4
Gagandeep Singh <g.singh@nxp.com>
2026-03-17 19:52:22 +0530

    bus/dpaa: fix device probe issue
    Allow device probing by removing the unintended return
    in the LS1043 SoC check
    
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/bus/dpaa/dpaa_bus.c
+++ drivers/bus/dpaa/dpaa_bus.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2017-2025 NXP
+ * Copyright 2017-2026 NXP
  */
 /* System headers */
 #include <stdio.h>
@@ -792,10 +792,8 @@ rte_dpaa_bus_probe(void)
 	}
 
 	/* Disabling the default push mode for LS1043A */
-	if (s_rte_dpaa_bus.svr_ver == SVR_LS1043A_FAMILY) {
+	if (s_rte_dpaa_bus.svr_ver == SVR_LS1043A_FAMILY)
 		s_rte_dpaa_bus.max_push_rxq_num = 0;
-		return 0;
-	}
 
 	penv = getenv("DPAA_PUSH_QUEUES_NUMBER");
 	if (penv)
