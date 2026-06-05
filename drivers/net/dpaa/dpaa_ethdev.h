3973546f5ddf8682f17d40dfd96db2898eab4075
Gagandeep Singh <g.singh@nxp.com>
2026-06-05 11:40:24 +0530

    update NXP copyrights
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/net/dpaa/dpaa_ethdev.h
+++ drivers/net/dpaa/dpaa_ethdev.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  *
  *   Copyright (c) 2014-2016 Freescale Semiconductor, Inc. All rights reserved.
- *   Copyright 2017-2024 NXP
+ *   Copyright 2017-2026 NXP
  *
  */
 #ifndef __DPAA_ETHDEV_H__
